812bede07046d8417265d6085023e1388ffd49f0
josh <jglam88@gmail.com>
2023-08-09 14:45:33 -0400

    creates for and while functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names, event) {
+  let newArray = []
+  for (let i = 0; i < names.length; i++) {
+  newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArray
+}
+ 
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i--);
+  }
+  return i
+}
+
+
+console.log(countDown(5))
